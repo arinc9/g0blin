1e0794480e0430d0825c6e019e81c1094e514c23
Arınç ÜNAL <arincunal@Arncs-iMac.local>
2018-01-31 16:08:44 +0300

    Updated dropbear with latest version (2017.75)

--- g0blin/offsets.m
+++ g0blin/offsets.m
@@ -634,31 +634,9 @@ iPad6,12 - iPad 5/2017 (Cellular)
     
     if(!strcmp(device, "iPad5,4") || !strcmp(device, "iPad5,3"))
     {
-        // 10.3.3
-        if(!strcmp(osversion, "14G5053A"))
-        {
-            OFFSET_ZONE_MAP                        = 0xfffffff007558478;
-            OFFSET_KERNEL_MAP                      = 0xfffffff0075b4050;
-            OFFSET_KERNEL_TASK                     = 0xfffffff0075b4048;
-            OFFSET_REALHOST                        = 0xfffffff00753aba0;
-            OFFSET_BZERO                           = 0xfffffff00708df80;
-            OFFSET_BCOPY                           = 0xfffffff00708ddc0;
-            OFFSET_COPYIN                          = 0xfffffff00718d120;
-            OFFSET_COPYOUT                         = 0xfffffff00718d314;
-            OFFSET_ROOTVNODE                       = 0xfffffff0075b40b0;
-            OFFSET_CHGPROCCNT                      = 0xfffffff00739ac28;
-            OFFSET_KAUTH_CRED_REF                  = 0xfffffff007374fb4;
-            OFFSET_IPC_PORT_ALLOC_SPECIAL          = 0xfffffff0070a60b4;
-            OFFSET_IPC_KOBJECT_SET                 = 0xfffffff0070b938c;
-            OFFSET_IPC_PORT_MAKE_SEND              = 0xfffffff0070a5bd8;
-            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB    = 0xfffffff006ecdd38;
-            OFFSET_ROP_ADD_X0_X0_0x10              = 0xfffffff006456174;
-            OFFSET_OSSERIALIZER_SERIALIZE          = 0xfffffff00744ddb4;
-            OFFSET_ROP_LDR_X0_X0_0x10              = 0xfffffff0063a4a84;
-        }
         
         // 10.3.3
-        if(!strcmp(osversion, "14G60"))
+        if(!strcmp(osversion, "14G60") || !strcmp(osversion, "14G5053A"))
         {
             OFFSET_ZONE_MAP                        = 0xfffffff007558478;
             OFFSET_KERNEL_MAP                      = 0xfffffff0075b4050;
